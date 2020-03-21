4782adfdb804f04b10a3cceb38f3c6224b92b989
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-03-21 19:15:37 -0400

    fluid images started working

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -1,15 +1,41 @@
 import React from "react"
 import { Link } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const Projects = () => (
-  <Layout>
-    <SEO title="Skills" />
-    <h1>Skills</h1>
-    <p>Skills page</p>
-  </Layout>
-)
+const Skills = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allFile(limit: 1) {
+        edges {
+          node {
+            childImageSharp {
+              id
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
 
-export default Projects
+  console.log("THIS IS IN THE SKILLS FOLDER", data)
+  return (
+    <Layout>
+      <SEO title="Skills" />
+      <h1>Skills</h1>
+      <p>Skills page</p>
+      <Img
+        fluid={data.allFile.edges[0].node.childImageSharp.fluid}
+        alt="Gatsby icon"
+      />
+    </Layout>
+  )
+}
+
+export default Skills
