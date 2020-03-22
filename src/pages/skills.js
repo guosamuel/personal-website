593099e959b208fc076754d7f2a2156bba7bbb0b
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-03-22 00:41:49 -0400

    adding the skill logos now

--- src/pages/skills.js
+++ src/pages/skills.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import { useStaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
+import styles from "./css-modules.module.css"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,7 +10,7 @@ import SEO from "../components/seo"
 const Skills = () => {
   const data = useStaticQuery(graphql`
     {
-      allFile(limit: 1) {
+      allFile(filter: {relativeDirectory: {eq: "icons"}}) {
         edges {
           node {
             childImageSharp {
@@ -25,15 +26,30 @@ const Skills = () => {
   `)
 
   console.log("THIS IS IN THE SKILLS FOLDER", data)
+
+  const icons = data.allFile.edges.map( icon => {
+    return (
+      <div key={icon.node.id}>
+        <Img
+          className={styles.icon}
+          fluid={icon.node.childImageSharp.fluid}
+          alt="Portfolio picture on the index page"
+        />
+      </div>
+    )
+  })
   return (
     <Layout>
       <SEO title="Skills" />
       <h1>Skills</h1>
       <p>Skills page</p>
-      <Img
+      <div className={styles.user}>
+        {icons}
+      </div>
+      {/*<Img
         fluid={data.allFile.edges[0].node.childImageSharp.fluid}
         alt="Gatsby icon"
-      />
+      />*/}
     </Layout>
   )
 }
