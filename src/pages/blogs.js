097aaebab39032e01da6c1dbcd0dfb5649bf5acd
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-04-16 18:17:32 -0400

    got the graphQL query to work for only the blogs folder

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -7,11 +7,12 @@ import SEO from "../components/seo"
 import BlogIndex from "../components/blogIndex"
 
 function Blogs({ data }){
-  const { edges } = data.allMarkdownRemark
+  console.log("I AM IN THE BLOG PAGE", data)
+  // const { edges } = data.allMarkdownRemark
 
-  const allBlogIndexes = edges.map( blog => {
-    const { path, date, title } = blog.node.frontmatter
-    const { id, excerpt } = blog.node
+  const allBlogIndexes = data.allMarkdownRemark.nodes.map( blog => {
+    const { path, date, title } = blog.frontmatter
+    const { id, excerpt } = blog
     return (
       <div key={id}>
         <BlogIndex path={path} date={date} title={title} excerpt={excerpt}/>
@@ -28,21 +29,38 @@ function Blogs({ data }){
   )
 }
 
+// export const blogIndexQuery = graphql`
+//   query blogIndexQuery {
+//     allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
+//       edges {
+//         node {
+//           id
+//           frontmatter {
+//             date(formatString: "MM-DD-YYYY")
+//             path
+//             title
+//           }
+//         excerpt(pruneLength: 1000)
+//         }
+//       }
+//     }
+//   }
+// `
+
 export const blogIndexQuery = graphql`
   query blogIndexQuery {
-    allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC}) {
-    edges {
-      node {
+    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}, filter: {fileAbsolutePath: {regex: "/blogs/"}}) {
+      nodes {
         id
         frontmatter {
+          title
           date(formatString: "MM-DD-YYYY")
           path
-          title
         }
         excerpt(pruneLength: 1000)
       }
     }
   }
-  }
 `
+
 export default Blogs
---
+++ src/pages/projects/ProgressingForward/index.md
@@ -0,0 +1,8 @@
+---
+github:  "https://github.com/guosamuel/progressing-forward-front-end"
+link: "https://progressingforward.netlify.app/"
+demo: "https://www.youtube.com/watch?v=qgSnM6FDT28&feature=youtu.be"
+title: "Progressing Forward"
+---
+
+A project progress tracker for users involved in their respective projects using React, Ruby on Rails, and Redux
