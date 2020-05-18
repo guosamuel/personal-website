ecfa0b96c43b2a0f0b9defdbd85cf928d07031e1
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-05-18 12:05:52 -0400

    adopted media queries for the header

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,13 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import styles from "./css-modules.module.css"
 
 const Header = ({ siteTitle }) => (
   <header
-    style={{
-      background: `black`,
-      marginBottom: `1.45rem`,
-    }}
+    className={styles.header}
   >
+    {/*
     <div
       style={{
         margin: `0 auto`,
@@ -20,11 +19,16 @@ const Header = ({ siteTitle }) => (
         fontFamily: "Jura"
       }}
     >
+    */}
+    <div className={styles.headerContainer}>
+      {/*
       <div style={{
         fontSize: (Math.max(window.innerWidth, window.innerHeight)*0.04) > 40 ? 40 : `4vmax`,
         marginTop: `0.5rem`,
         marginBottom: `0.5rem`
       }}>
+      */}
+      <div className={styles.name}>
       <p style={{ margin: 0 }}>
         <Link
           to="/"
