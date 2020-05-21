71c099611cb9b6b65d0faed472ebf7767646cdcf
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-05-21 11:42:02 -0400

    cleaned up the blog page a little bit

--- src/components/blogIndex.js
+++ src/components/blogIndex.js
@@ -10,7 +10,19 @@ const BlogIndex = ({ path, date, title, excerpt }) => {
     <br />
     <br />
     <p>{excerpt}</p>
-
+    <div style={{ display: 'flex', justifyContent: 'flex-end'}}>
+      <button
+        onClick={() => {window.open(path)}}
+        style={{
+          backgroundColor: 'blue',
+          color: 'white',
+          fontFamily: 'sans-serif'
+        }}
+      >
+        Read More
+      </button>
+    </div>
+    <br />
     <hr />
     </>
   )
