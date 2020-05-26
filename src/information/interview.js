925798d9f1c9181b7afb9c13a47b354baaf3a042
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-05-26 17:33:03 -0400

    shortened interview answers

--- src/information/interview.js
+++ src/information/interview.js
@@ -5,27 +5,27 @@ export const interview = [
   },
   {
     question: "How did you end up in your current career?",
-    answer: "Funnily enough, I wasn’t always a software developer. I got my Bachelor of Science in Mechanical Engineering at Columbia University in 2014. From there, I was a Fluid Systems Engineer for a few years, and then as a Field and Project Engineer in a construction and signal firm that works closely with the MTA. I decided to take a leap of faith to pursue coding as a software developer because it looked like technology, and code for that matter, will be at the forefront for the next upcoming years. So far, I’m loving code. Even when I’m not working on it, I would catch myself thinking about it."
+    answer: "Funnily enough, I wasn’t always a software developer. I got my Bachelor of Science in Mechanical Engineering in 2014. I was in the engineering field for about 5 years. I decided to take a leap of faith to pursue coding as a software developer because it looked like technology, and code for that matter, will be at the forefront for the next upcoming years."
   },
   {
     question: "Weren’t you scared about taking that leap? What would you have done if it didn't work out?",
-    answer: "Of course I was scared. Who wouldn’t be if they don’t know what’s going to happen next? If this doesn’t work, then I definitely gained a ton of new skills and a new hobby. Overall, this strengthened my skill set to combine engineering and coding, so I’m not too worried about the future."
+    answer: "Of course I was scared. Who wouldn’t be if they don’t know what’s going to happen next? If this doesn’t work, then I gained a ton of new skills and a new hobby. Overall, this strengthened my skill set to combine engineering and coding, so I’m not too worried about the future."
   },
   {
     question: "Outside of your career, what do you do for fun?",
-    answer: "Oh, a myriad of things. It can be playing volleyball, trying out new restaurants, attending concerts, or attending conventions like Comic Con. Essentially, whatever the city has to offer, I would try to take advantage of that. I used to juggle and do archery as well so I’ve been thinking about getting back into those activities."
+    answer: "Oh, a myriad of things. It can be playing volleyball, trying out new restaurants, attending concerts, or attending conventions like Comic Con. I used to juggle and do archery as well so I’ve been thinking about getting back into those activities."
   },
   {
     question: "Seems like you’re just about to try anything. What are some things on your bucket list that you haven’t gotten around to yet?",
-    answer: "That’s a great question. Skydiving is definitely one of them, even though I don’t do well with heights. Archery tag is also on that list. It’s like paintball but you use bow and arrows to take out your opponent. Don’t worry, the tips of the arrows are replaced with a flat cylinder so it’s definitely safe for everyone. Probably explore the states a bit more too. I think I travelled outside the states more than I have within the states."
+    answer: "That’s a great question. Skydiving is one of them, even though I don’t do well with heights. Archery tag is also on that list which is like paintball but with bows and arrows and the tips are covered. Probably explore the states a bit more too where their food is famous, like New Orleans for cajun food."
   },
   {
-    question: "What are your favorite quotes and why are they your favorite?",
-    answer: "I actually have two. The first one is ‘Trust me, I’m an engineer’ and the second is ‘Don’t worry about it’. I use ‘Trust me, I’m an engineer’ a lot whenever I talk with non-engineers in the most ironic way possible. From what I’ve experienced, for some reason, people think engineers are like these super geniuses when in reality we’re just your average person, the same as you and me. With ‘Don’t worry about it’, this was my motto growing up throughout high school and even to this day. As a teenager, the pressure of doing academically well, maintaining a social life, staying healthy, applying for colleges, attending extracurriculars, on top of developing emotionally mature was a lot to handle. That phrase helped me recognize you can only do so much at the time, so as long as you put in the effort, everything else should play out naturally."
+    question: "What is your favorite quote and why?",
+    answer: "It would be “Don’t worry about it.” This was my motto when I was a teenager and it trickled down into adulthood. As a teenager, the pressure of doing academically well, maintaining a social life, staying healthy, applying for colleges, attending extracurriculars, and developing emotionally was a lot to handle. That phrase helped me recognize you can only do so much at once, so as long as you put in the effort, everything else should play out naturally."
   },
   {
     question: "Wow, who would’ve thought a simple phrase like that would’ve stuck and motivated you to this day. All right, I think it’s about time we wrapped this up. Any last words you have for those out there reading this right now?",
-    answer: "First, thank you for reading the entire page. Hopefully I was able to shed some light on who I am as a person. Second, if you’re interested in checking out my work or blogs, please click on the appropriate link on the top right corner of this page."
+    answer: "First, thank you for reading the entire page. Hopefully I was able to shed some light on who I am. Second, if you’re interested in checking out my work or blogs, please click on the appropriate link on the top right corner of this page. Lastly, if you would like to connect, my contact information is in the footer so feel free to reach out any time!"
   }
 
 ]
