ea0d916acae50f716e17134c961119991ce8f139
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-06-01 12:22:33 -0400

    completed the 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,61 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from 'gatsby-image'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import styles from "./css-modules.module.css"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  // const wheatly = useStaticQuery(graphql`
+  // {
+  //   imageSharp(fluid: {originalName: {eq: "wheatly.png"}}) {
+  //     id
+  //     fluid {
+  //       ...GatsbyImageSharpFluid
+  //     }
+  //   }
+  // }
+  // `)
+  const data = useStaticQuery(graphql`
+    {
+      allFile(filter: {relativeDirectory: {eq: "404"}}) {
+        edges {
+          node {
+            childImageSharp {
+              id
+              fluid {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+        }
+      }
+    }`)
+
+  const wheatlyID = data.allFile.edges[0].node.childImageSharp
+  const wheatlyImage = data.allFile.edges[0].node.childImageSharp.fluid
+  const cakeID = data.allFile.edges[1].node.childImageSharp.fluid.id
+  const cakeImage = data.allFile.edges[1].node.childImageSharp.fluid
+
+  return (
+    <Layout>
+      <SEO title="404: Not found" />
+      <div className={styles.errorContainer}>
+        <div className={styles.errorNumberContainer}>
+          <span>4</span>
+          <Img id={wheatlyID} className={styles.errorImage} fluid={wheatlyImage} />
+          <span>4</span>
+        </div>
+        <br/>
+        <p className={styles.errorText}>The page is a lie!</p>
+        <Img id={cakeID} className={styles.cakeImage} fluid={cakeImage} />
+        <p className={styles.errorText}>But good thing this cake isn't!</p>
+
+
+      </div>
+    </Layout>
+  )
+}
 
 export default NotFoundPage
