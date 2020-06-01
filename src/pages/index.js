7cab94fd78dde4c9083d25ae836eb4574409881b
Samuel Guo <47992471+guosamuel@users.noreply.github.com>
2020-06-01 17:45:25 -0400

    refactored the Home page with its CSS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,11 @@
 import React from "react"
-import styles from "./css-modules.module.css"
 import Img from "gatsby-image"
 import { useStaticQuery, graphql } from "gatsby"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-// import Image from "../components/image"
 import SEO from "../components/seo"
-
-const imageURL = 'https://media-exp1.licdn.com/dms/image/C5603AQH9KmTFZP-zQA/profile-displayphoto-shrink_200_200/0?e=1588809600&v=beta&t=fSXinxjcWcKo-vKhZLw8MJlyy18ELJxzQnZQ-UD_cV4'
-// <img src={imageURL} className={styles.avatar} alt="" />
+import styles from "./css-modules.module.css"
 
 const IndexPage = () => {
   const data = useStaticQuery(graphql`
@@ -29,84 +25,64 @@ const IndexPage = () => {
     }
   `)
 
-  console.log(data)
-
   return(
-  <Layout>
-    <SEO title="Home" />
-    <div className={styles.user}>
-      <Img
-        className={styles.picture}
-        fluid={data.allFile.edges[0].node.childImageSharp.fluid}
-        alt="Portfolio picture on the index page"
-      />
-      <div className={styles.description}>
-        <p className={styles.username}>Hi, Samuel here!</p>
-        <p className={styles.excerpt}>
-          You came here looking for something. Let’s help you find it.
-        </p>
-        <br />
-        <p>
-          Is it my work?
-          <Link
-            to="/projects"
-            className={styles.indexLinks}
-          >
-            Click here!
-          </Link>
-        </p>
-        <p>
-          Or is it my skills?
-          <Link
-            to="/skills"
-            className={styles.indexLinks}
-          >
-            Check them out!
-          </Link>
-        </p>
-        <p>
-          Perhaps you’re curious about the insight I gathered?
-          <Link
-            to="/blogs"
-            className={styles.indexLinks}
-          >
-            Here are some of my blogs!
-          </Link>
-        </p>
-        <p>
-          Maybe more details about me?
-          <Link
-            to="/about"
-            className={styles.indexLinks}
-          >
-            Right this way!
-          </Link>
-        </p>
-        <p>
-          Can’t find what you’re looking for? Let’s get in touch so I can help!
-        </p>
+    <Layout>
+      <SEO title="Home" />
+      <div className={styles.user}>
+        <Img
+          className={styles.picture}
+          fluid={data.allFile.edges[0].node.childImageSharp.fluid}
+          alt="Portfolio picture on the home page"
+        />
+        <div className={styles.description}>
+          <p className={styles.username}>Hi, Samuel here!</p>
+          <p className={styles.excerpt}>
+            You came here looking for something. Let’s help you find it.
+          </p>
+          <br />
+          <p>
+            Is it my work?
+            <Link
+              to="/projects"
+              className={styles.pageLinks}
+            >
+              Click here!
+            </Link>
+          </p>
+          <p>
+            Or is it my skills?
+            <Link
+              to="/skills"
+              className={styles.pageLinks}
+            >
+              Check them out!
+            </Link>
+          </p>
+          <p>
+            Perhaps you’re curious about the insight I gathered?
+            <Link
+              to="/blogs"
+              className={styles.pageLinks}
+            >
+              Here are some of my blogs!
+            </Link>
+          </p>
+          <p>
+            Maybe more details about me?
+            <Link
+              to="/about"
+              className={styles.pageLinks}
+            >
+              Right this way!
+            </Link>
+          </p>
+          <p>
+            Can’t find what you’re looking for? Let’s get in touch so I can help!
+          </p>
+        </div>
       </div>
-    </div>
-  </Layout>
+    </Layout>
   )
 }
 
-// export const portfolioPictureQuery = graphql`
-// query portfolioPictureQuery {
-//   allImageSharp(filter: {fixed: {originalName: {eq: "portfolio_picture.jpg"}}}) {
-//     edges {
-//       node {
-//         fixed {
-//           base64
-//           tracedSVG
-//           aspectRatio
-//           srcWebp
-//           srcSetWebp
-//           originalName
-//         }
-//       }
-//     }
-//   }
-// }
-// `
 export default IndexPage
